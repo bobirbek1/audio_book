d8eaf3d915b898716cd62668d4fc3cbf3e2046ff
BobirbekJafton <bobirbek.a@jafton.com>
2023-11-24 07:24:32 +0500

    feat: build bottom navigation bar

--- lib/src/presentation/base/theme_provider.dart
+++ lib/src/presentation/base/theme_provider.dart
@@ -9,60 +9,70 @@ class ThemeProvider extends ChangeNotifier {
   }
 
   final _light = ThemeData(
-    brightness: Brightness.light,
-    fontFamily: GoogleFonts.poppins().fontFamily,
-    scaffoldBackgroundColor: ColorName.white,
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: ColorName.primary50,
-        minimumSize: const Size.fromHeight(56),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            8,
+      brightness: Brightness.light,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      scaffoldBackgroundColor: ColorName.white,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: ColorName.primary50,
+          minimumSize: const Size.fromHeight(56),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              8,
+            ),
           ),
         ),
       ),
-    ),
-    outlinedButtonTheme: OutlinedButtonThemeData(
-      style: OutlinedButton.styleFrom(
-        foregroundColor: ColorName.primary50,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            8,
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: ColorName.primary50,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              8,
+            ),
+          ),
+          side: const BorderSide(
+            color: ColorName.primary50,
+            width: 1,
+          ),
+          minimumSize: const Size.fromHeight(
+            56,
           ),
         ),
-        side: const BorderSide(
-          color: ColorName.primary50,
-          width: 1,
-        ),
-        minimumSize: const Size.fromHeight(
-          56,
-        ),
       ),
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            8,
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              8,
+            ),
           ),
+          minimumSize: const Size.fromHeight(56),
+          foregroundColor: ColorName.primary50,
         ),
-        minimumSize: const Size.fromHeight(56),
-        foregroundColor: ColorName.primary50,
       ),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      hintStyle: LightTextStyles().medium14.copyWith(
+      inputDecorationTheme: InputDecorationTheme(
+          hintStyle: LightTextStyles().medium14.copyWith(
                 color: ColorName.neutral40,
               ),
-      fillColor: ColorName.neutral5 ,
-      filled: true,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8),
-        borderSide: BorderSide.none,
-      )
-    ),
-  );
+          fillColor: ColorName.neutral5,
+          filled: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide.none,
+          )),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: ColorName.white,
+        elevation: 8,
+        selectedItemColor: ColorName.primary50,
+        unselectedItemColor: ColorName.neutral60,
+        selectedLabelStyle: LightTextStyles().semiBold10.copyWith(
+              color: ColorName.primary50,
+            ),
+        unselectedLabelStyle: LightTextStyles().regular10.copyWith(
+              color: ColorName.neutral60,
+            ),
+      ));
 
   final _dark = ThemeData(
     brightness: Brightness.dark,
--- lib/src/presentation/pages/main_page/main_page.dart
+++ lib/src/presentation/pages/main_page/main_page.dart
@@ -1,22 +1,80 @@
-import 'package:audio_book/src/presentation/base/theme_provider.dart';
-import 'package:audio_book/src/presentation/widgets/regular_elevated_button.dart';
+import 'package:audio_book/gen/assets.gen.dart';
+import 'package:audio_book/gen/colors.gen.dart';
+import 'package:audio_book/src/presentation/pages/main_page/home_page.dart';
+import 'package:audio_book/src/presentation/pages/main_page/library_page.dart';
+import 'package:audio_book/src/presentation/pages/main_page/search_page.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RegularElevatedButton(
-          onPressed: () {
-            context.read<ThemeProvider>().changeTheme();
+      body: switch (_currentIndex) {
+        0 => const HomePage(),
+        1 => const SearchPage(),
+        2 => const LibraryPage(),
+        _ => const SizedBox(),
+      },
+      bottomNavigationBar: DecoratedBox(
+        decoration: const BoxDecoration(boxShadow: [
+          BoxShadow(
+            color: ColorName.neutral10,
+            blurRadius: 8,
+            spreadRadius: 6,
+            offset: Offset(0, -2),
+          )
+        ]),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
           },
-          text: "Change theme",
+          items: [
+            _getBottomNavItem(Assets.icons.homeOutline.path,
+                Assets.icons.homeBold.path, "Home"),
+            _getBottomNavItem(Assets.icons.searchOutline.path,
+                Assets.icons.searchBold.path, "Search"),
+            _getBottomNavItem(Assets.icons.document.path,
+                Assets.icons.documentBold.path, "Library"),
+          ],
         ),
       ),
     );
+  }
+
+  BottomNavigationBarItem _getBottomNavItem(
+      String icon, String activeIcon, String label) {
+    return BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          icon,
+          height: 24,
+          width: 24,
+          colorFilter: const ColorFilter.mode(
+            ColorName.neutral60,
+            BlendMode.srcIn,
+          ),
+        ),
+        activeIcon: SvgPicture.asset(
+          activeIcon,
+          height: 24,
+          width: 24,
+          colorFilter: const ColorFilter.mode(
+            ColorName.primary50,
+            BlendMode.srcIn,
+          ),
+        ),
+        label: label);
   }
 }
