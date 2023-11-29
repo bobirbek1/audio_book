64dc221580174923eb25460f06c0f999aae73a71
BobirbekJafton <bobirbek.a@jafton.com>
2023-11-29 22:13:43 +0500

    feat: add file cache feature for reading page

--- lib/src/presentation/pages/reading_page/reading_page.dart
+++ lib/src/presentation/pages/reading_page/reading_page.dart
@@ -1,14 +1,45 @@
+import 'dart:io';
+
 import 'package:audio_book/gen/assets.gen.dart';
 import 'package:audio_book/src/helpers/extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class ReadingPage extends StatelessWidget {
+class ReadingPage extends StatefulWidget {
   const ReadingPage({super.key});
 
   @override
+  State<ReadingPage> createState() => _ReadingPageState();
+}
+
+class _ReadingPageState extends State<ReadingPage> {
+  Map<String, dynamic>? args;
+
+  File? pdfFile;
+
+  @override
+  void initState() {
+    Future.microtask(() {
+      DefaultCacheManager()
+          .getSingleFile(args?["file_url"], key: args?["name"])
+          .then((value) {
+        setState(() {
+          pdfFile = value;
+        });
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    args = context.getArguments<Map<String, dynamic>?>();
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final args = context.getArguments<Map<String, dynamic>?>();
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -30,11 +61,15 @@ class ReadingPage extends StatelessWidget {
             ? const Center(
                 child: Text("No book file url provided!"),
               )
-            : SfPdfViewer.network(
-                args!["file_url"]!,
-                pageLayoutMode: PdfPageLayoutMode.single,
-                scrollDirection: PdfScrollDirection.horizontal,
-              ),
+            : pdfFile == null
+                ? const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  )
+                : SfPdfViewer.file(
+                    pdfFile!,
+                    pageLayoutMode: PdfPageLayoutMode.single,
+                    scrollDirection: PdfScrollDirection.horizontal,
+                  ),
       ),
     );
   }
